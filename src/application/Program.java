373f626aa17209e6b81646e9bf5ab87df01ed09d
Thiago Carvalho <thiagoaccarv@gmail.com>
2025-12-02 12:37:57 -0300

    Reusing instantiation & findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -13,9 +14,17 @@ public class Program {
 
        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-       Seller seller = sellerDao.findById(3);
-
+        System.out.println("=== TEST 1: seller findById ===");
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list) {
+            System.out.println(obj);
+        }
+
     }
 }
 
